896841b79ca9c26e8c94cbbe40dc999179219d66
Roland Meertens <rolandmeertens@gmail.com>
2021-05-14 13:25:11 +0200

    🏎️added Arduino requirements to make the motor drive

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mega_useful_robot
+
+
+
+Dependencies: 
+https://github.com/pcdangio/ros-driver_mpu9250
+git@github.com:pcdangio/ros-sensor_msgs_ext.git
+http://abyz.me.uk/rpi/pigpio/download.html
+
+https://github.com/wolfeidau/ros-mpu9250-node
+	* https://github.com/eclipse/upm/blob/master/docs/installing.md
+	* https://github.com/eclipse/mraa
+
+
+
+
+http://wiki.ros.org/rosserial_arduino/Tutorials/Arduino%20IDE%20Setup
---
+++ arduino/mega_useful/mega_useful.ino
@@ -0,0 +1,64 @@
+
+#include <ros.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/UInt8.h>
+#include <Servo.h>
+
+ros::NodeHandle  nh;
+
+int left_pin = 8; 
+int right_pin = 9;
+int delaytime = 15; // Time delay between reading messages
+int initialised = 0;
+unsigned long time_since_start = 0;
+
+char showreceived[] = "received";
+
+Servo leftservo;  // create servo object to control a servo
+Servo rightservo;  // create servo object to control a servo
+
+
+void leftmotormessage( const std_msgs::UInt8& motor_msg){
+  nh.logfatal(showreceived);
+  if(motor_msg.data <= 180){
+    leftservo.write(motor_msg.data);
+  }
+
+}
+
+void rightmotormessage( const std_msgs::UInt8& motor_msg){
+  nh.logfatal(showreceived);
+  if(motor_msg.data <= 180){
+    rightservo.write(motor_msg.data);
+  }
+
+}
+
+ros::Subscriber<std_msgs::UInt8> sub_left("left_motor", &leftmotormessage );
+ros::Subscriber<std_msgs::UInt8> sub_right("right_motor", &rightmotormessage );
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  leftservo.attach(left_pin, 1000, 2000);  // 1000 and 2000 are values for the ESCS I'm using
+  leftservo.write(90); // Value goes between 0 and 180 for a servo. 90 means neutral if your esc can go forwards and backwards
+
+  rightservo.attach(right_pin, 1000, 2000);
+  rightservo.write(90);
+
+  nh.initNode();
+  nh.subscribe(sub_left);
+  nh.subscribe(sub_right);
+}
+
+
+// Make sure the servos are set correctly to zero before going on ROS
+void loop() {
+  if (initialised > 3){
+    nh.spinOnce();
+  }
+  else{
+    initialised += 1;
+    delay(1000);
+  }
+}
